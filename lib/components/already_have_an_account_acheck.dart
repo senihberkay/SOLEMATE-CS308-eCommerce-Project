fbe003e708a3f2a02941a8d77a9c346bd722a8c6
Senih <sberkay@sabanciuniv.edu>
2022-04-04 14:36:11 +0300

    assets were updated

--- lib/components/already_have_an_account_acheck.dart
+++ lib/components/already_have_an_account_acheck.dart
@@ -17,7 +17,7 @@ class AlreadyHaveAnAccountCheck extends StatelessWidget {
       children: <Widget>[
         Text(
           login ? "Don’t have an Account ? " : "Already have an Account ? ",
-          style: const TextStyle(color: kPrimaryColor),
+          style: const TextStyle(color: Colors.black),
         ),
         GestureDetector(
           onTap: press,
--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
 
-const kPrimaryColor = Color(0xFF6F35A5);
-const kPrimaryLightColor = Color(0xFFF1E6FF);
+const kPrimaryColor = Color.fromARGB(255, 255, 127, 49);
+const kPrimaryLightColor = Color.fromARGB(27, 183, 182, 182);
