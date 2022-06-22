57e18a73593a92107f7205850f93439b54129f77
Senih Berkay Akın <sberkay@sabanciuniv.edu>
2022-06-22 04:17:11 +0300

    settings page was updated

--- lib/Screens/settings/components/body.dart
+++ lib/Screens/settings/components/body.dart
@@ -3,30 +3,134 @@ import 'package:flutter_auth/size_config.dart';
 
 import '../../../constants.dart';
 
+// class Body extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return SizedBox(
+//       width: double.infinity,
+//       child: SingleChildScrollView(
+//         child: Padding(
+//           padding:
+//               EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+//           child: Column(
+//             crossAxisAlignment: CrossAxisAlignment.start,
+//             children: [
+//               SizedBox(height: SizeConfig.screenHeight * 0.04),
+//               Text(
+//                 "Account Settings",
+//                 style: TextStyle(
+//                   fontSize: getProportionateScreenWidth(22),
+//                   color: Colors.black,
+//                   fontWeight: FontWeight.bold,
+//                   height: getProportionateScreenHeight(0.01),
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
-      child: SingleChildScrollView(
-        child: Padding(
-          padding:
-              EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: SizeConfig.screenHeight * 0.04),
-              Text(
-                "Account Settings",
-                style: TextStyle(
-                  fontSize: getProportionateScreenWidth(22),
+    return Scaffold(
+      body: Container(
+        padding:
+            EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+        child: ListView(
+          children: [
+            Text(
+              "Settings",
+              style: TextStyle(
+                  fontSize: getProportionateScreenWidth(25),
                   color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                  height: getProportionateScreenHeight(0.01),
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: getProportionateScreenHeight(40),
+            ),
+            Row(
+              children: [
+                Icon(
+                  Icons.person,
+                  color: kPrimaryColor,
                 ),
+                SizedBox(
+                  width: getProportionateScreenWidth(6),
+                ),
+                Text(
+                  "Account",
+                  style: TextStyle(
+                      fontSize: getProportionateScreenWidth(17),
+                      fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+            Divider(
+              height: getProportionateScreenHeight(15),
+              thickness: 2,
+            ),
+            SizedBox(
+              height: getProportionateScreenHeight(10),
+            ),
+            buildAccountOptionRow(context, "Change password"),
+            buildAccountOptionRow(context, "Content settings"),
+            buildAccountOptionRow(context, "Social"),
+            buildAccountOptionRow(context, "Language"),
+            buildAccountOptionRow(context, "Privacy and security"),
+          ],
+        ),
+      ),
+    );
+  }
+
+  GestureDetector buildAccountOptionRow(BuildContext context, String title) {
+    return GestureDetector(
+      onTap: () {
+        showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text(title),
+                content: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text("Option 1"),
+                    Text("Option 2"),
+                    Text("Option 3"),
+                  ],
+                ),
+                actions: [
+                  FlatButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Text("Close")),
+                ],
+              );
+            });
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              title,
+              style: TextStyle(
+                fontSize: getProportionateScreenWidth(16),
+                fontWeight: FontWeight.w500,
+                color: Colors.grey[600],
               ),
-            ],
-          ),
+            ),
+            Icon(
+              Icons.arrow_forward_ios,
+              color: Colors.grey,
+            ),
+          ],
         ),
       ),
     );
